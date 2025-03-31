11bf1a38842c0dd09062766380dfc3277dacf7be
Jake Gaylor <jhgaylor@gmail.com>
2025-03-31 13:50:47 -0500

    add rss feed

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,26 @@
+import { feedPlugin } from "@11ty/eleventy-plugin-rss";
+
+function withRSS(eleventyConfig) {
+    eleventyConfig.addPlugin(feedPlugin, {
+		type: "atom",
+		outputPath: "/feed.xml",
+		collection: {
+			name: "posts", // iterate over `collections.posts`
+			limit: 0,     // 0 means no limit
+		},
+		metadata: {
+			language: "en",
+			title: "Jake Gaylor",
+			subtitle: "My thoughts, on paper.",
+			base: "https://jakegaylor.com/",
+			author: {
+				name: "Jake Gaylor",
+				email: "jhgaylor@gmail.com",
+			}
+		}
+	});
+}
+
 export default function(eleventyConfig) {
     // Copy assets to their respective directories
     eleventyConfig.addPassthroughCopy({ "_assets/css": "css" });
